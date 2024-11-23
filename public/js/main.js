3acb10b9e9112027417fecbaf1b6c93a408ae717
Brian McGee <92900596+brmcgee@users.noreply.github.com>
2024-11-22 21:53:02 -0500

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -142,7 +142,7 @@ function htmlFetchAllCustomer(data){
     })
 
     html += `</div>`;
-    // html += modal('All Jobs');
+    html += modal('Customer Contact');
     return html;
 
 }
@@ -157,8 +157,8 @@ function jobCard(data){
             <p class="card-text m-0 p-0">Phone: ${data.jPhone}</p>
             <p class="card-text m-0 p-0">${data.jAddress} - ${data.jCity}</p>
             <p class="card-text m-0 p-0">${getIcon(data.status)}${data.status}</p>
-            <a href="https://office.boxcar.site/${data.jImg || ''}">Img</a>
-            <a href="https://office.boxcar.site/${data.jScope || ''}  ">Scope</a>
+            <a href="${data.jImg || ''}">Img</a>
+            <a href="${data.jScope || ''}  ">Scope</a>
         </div>
         </div>
     
@@ -193,7 +193,8 @@ function jobBtn(custId){
             </button>`;
 }
 function customerBtn(custId){
-    return ` <button onclick="alert(this.id)" id="${custId}" class="btn ms-2 text-black" type="button">
+    return ` <button onclick="fetchCustomerModal('customerModalBody'), populateCustomerModal(${custId})" id="${custId}" class="btn ms-2 text-black" type="button"
+                data-bs-toggle="modal" data-bs-target="#customerModal">
                 <img class="pb-1" src="public/assets/icons/contact-green.png" alt="" width="25">
                     Profile
             </button>`;
@@ -219,27 +220,169 @@ function alertMessage(type, message) {
     
     `;
 }
-// function modal(title){
-//     return `
-//         <div class="modal fade" id="allJobsModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-//         <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-//             <div class="modal-content">
-//             <div class="modal-header">
-//                 <h1 class="modal-title fs-5" id="allJobsModalLabel">${title}</h1>
-//                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-//             </div>
-//             <div class="modal-body" id="allJobsModalBody">
-//                 ...
-//             </div>
-//             <div class="modal-footer">
-//                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-//                 <button type="button" class="btn btn-primary">Understood</button>
-//             </div>
-//             </div>
-//         </div>
-//         </div>
-//     `;
-// }
+function modal(title){
+    return `
+        <div class="modal fade" id="customerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable ">
+            <div class="modal-content rounded-0 ">
+            <div class="modal-header bm-bg-header rounded-0 text-white">
+                <h1 class="modal-title fs-5" id="customerModalLabel">${title}</h1>
+                <button type="button" class="btn-close text-white bg-white" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+                <div class="modal-body" id="customerModalBody">
+                    ${loader('primary', 'Loading contact info')}
+                </div>
+            </div>
+        </div>
+        </div>
+    `;
+}
+
+function fetchCustomerModal(target){
+    let t = document.getElementById(target);
+    let html = `
+    
+    <div id="addCustomerForm" class="m-0 p-0">
+       <div class="add-customer col-12 mx-auto bm-page">
+
+           <form action="http://localhost:5200/add-customer" method="POST" id="addCustomer">
+   
+               <div class="row pb-2 pt-3">
+   
+                   <div class="col-sm-12 col-md-3">
+                       <label for="fname" class="form-label">First:</label>
+                       <input type="text" class="form-control" id="fname" name="fname" required>
+                   </div>
+           
+                   <div class="col-sm-12 col-md-3">
+                       <label for="lname" class="form-label">Last:</label>
+                       <input type="text" class="form-control" id="lname" name="lname" required>
+                   </div>
+   
+                   <div class="col-sm-8 col-md-4">
+                       <label for="date" class="form-label">Date:</label>
+                       <input type="date" class="form-control" id="date" required name="date">
+                   </div>
+
+                   <div class="col-sm-4 col-md-2">
+                       <label for="custId" hidden class="form-label">CustId:</label>
+                       <input type="text" hidden class="form-control" id="custId" value=""  name="custId" disabled>
+                   </div>
+   
+               </div>
+   
+               <div class="row pb-2">
+   
+                   <div class="col-12 col-sm-6">
+                       <label for="phone" class="form-label">Phone:</label>
+                       <input type="tel" class="form-control" id="phone" required placeholder="Format: 123-456-7890" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}">
+                   </div>
+   
+                   <div class="col-12 col-sm-6">
+                       <label for="cell" class="form-label">Cell:</label>
+                       <input type="tel" class="form-control" id="cell" placeholder="Format: 123-456-7890" name="cell" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}">
+                   </div>
+   
+   
+               </div>
+   
+               <div class="row pb-2">
+
+                   <div class="col-sm-12 col-md-6">
+                       <label for="address" class="form-label">Address:</label>
+                       <input type="text" class="form-control" id="address" required name="address">
+                   </div>
+
+                   <div class="col-sm-12 col-md-6">
+                       <label for="email" class="form-label">Email:</label>
+                       <input type="email" class="form-control" id="email" name="email">
+                   </div>
+      
+               </div>    
+               
+               <div class="row pb-2">
+                   <div class="col-sm-12 col-md-6">
+                       <label for="city" class="form-label">City:</label>
+                       <input type="text" class="form-control" id="city" name="city">
+                   </div>
+   
+                   <div class="col-sm-6 col-md-3">
+                       <label for="state" class="form-label">State:</label>
+                       <input type="text" class="form-control" id="state" name="state">
+                   </div>   
+                   
+                   <div class="col-sm-6 col-md-3">
+                       <label for="zip" class="form-label">Zip:</label>
+                       <input type="text" class="form-control" id="zip"  name="zip">
+                   </div>        
+               </div>
+   
+               <div class="row pb-2">
+                   <div class="col-sm-12">
+                       <label for="notes" class="form-label">Notes:</label>
+                       <textarea type="text" class="form-control" id="notes" placeholder="Notes" name="notes" rows="13"></textarea>
+                   </div>
+   
+   
+               </div>
+               <div class="form-footer d-flex justify-content-end">
+                   <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                   <button type="button" class="btn btn-secondary me-1" onclick="clearCustomerForm()">Clear</button>
+                   <button type="button" onclick="handleAddCustomer()" class="btn btn-primary">Submit</button>
+               </div>
+   
+         </form>
+   
+       </div>
+   </div>   
+
+
+    `;
+    t.innerHTML = html;
+}
+
+async function populateCustomerModal(custId) {
+    
+    let url = `${pre}/customers/${custId}`
+    try {
+        let response = await fetch(url);
+        try {
+            let data = await response.json()
+            handlePopulateCustomerModal(data);
+        } catch (parseError) {
+            alertMessage('warning', parseError)
+        }
+    } catch (networkError) {
+        alertMessage('warning', networkError)
+    }
+}
+function handlePopulateCustomerModal(data) {
+    el('#fname').value = data[0].fname;
+    el('#lname').value = data[0].lname;
+    el('#address').value = data[0].address;
+    el('#city').value = data[0].city;
+    el('#state').value = data[0].state;
+    el('#zip').value = data[0].zip;
+    el('#phone').value = data[0].phone;
+    el('#cell').value = data[0].cell;
+    el('#notes').value = data[0].notes;
+    el('#email').value = data[0].email;
+    el('#custId').value = data[0].custId;
+    el('#date').value = data[0].date;
+}
+function clearCustomerForm() {
+    el('#fname').value = '';
+    el('#lname').value = '';
+    el('#address').value = '';
+    el('#city').value = '';
+    el('#state').value = '';
+    el('#zip').value = '';
+    el('#phone').value = '';
+    el('#cell').value = '';
+    el('#notes').value = '';
+    el('#email').value = '';
+    el('#custId').value = '';
+}
 function getIcon(status){
 
     if (status == 'Scheduled') {
